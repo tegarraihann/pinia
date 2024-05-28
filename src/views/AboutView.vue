32fbecc194f84c7944b676ee42c40a0d07787c1e
tegarraihann <tegar.raihann01@gmail.com>
2024-05-29 00:58:52 +0700

    Belajar Pinia

---
+++ src/views/AboutView.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>Menggunakan Store</h1>
+  <p>Akses count: {{ todoStore.count }}</p>
+  <p>Akses doubleCount : {{ todoStore.doubleCount }}</p>
+  <button @click="todoStore.increment">+</button><br /><br />
+
+  <h1>Tanpa Store</h1>
+  <p>Akses count tanpa store: {{ count }}</p>
+  <button @click="increment">+</button>
+</template>
+
+<script>
+import { useTodoStore } from '../stores/todoStore'
+import { ref } from 'vue'
+ export default {
+  setup() {
+      const count = ref(0)
+      const todoStore = useTodoStore()
+
+      return { todoStore, count }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script> -->
